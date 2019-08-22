a4b4e8ff824a8f2a489174c888266a050363ce49
A08019DIRP_C2E.03.06 <A08019DIRP_C2E.03.06@accenture.com>
2019-08-22 11:36:48 +0530

    Problem Set 01

---
+++ Problem Set 01/solutions.sql
@@ -0,0 +1,46 @@
+select * from guest;
+select * from booking;
+select * from hotel;
+select * from room;
+
+#1.List full details of all hotels.
+select * from hotel;
+
+#2.List full details of all hotels in New York.
+select * from hotel where city='New York';
+
+#3.List the names and cities of all guests, ordered according to their cities.
+select name,city from guest order by city;
+
+#4.List all details for non-smoking rooms in ascending order of price.
+select * from room where rtype='N';
+
+#5.List the number of hotels there are.
+select count(*) from hotel;
+
+#6.List the cities in which guests live. Each city should be listed only once.
+select  distinct city from guest ;
+
+#7.List the average price of a room.
+select avg(price) from room;
+
+#8.List hotel names, their room numbers, and the type of that room.
+select h.name as Hotel_name,r.room_no,r.rtype from hotel h inner join room r on h.hotel_No=r.hotel_No;
+
+#9.List the hotel names, booking dates, and room numbers for all hotels in New York.
+select h.name as Hotel_name,b.room_no,b.Date_From,b.Date_To from booking b inner join hotel h on b.hotel_No=h.hotel_No;
+
+#10.What is the number of bookings that started in the month of September?
+select count(*) as bookings_in_september from booking where month(Date_From)=9;
+
+#11.List the names and cities of guests who began a stay in New York in August.
+select g.name,g.city from  guest as g 
+inner join booking as b on g.guest_No=b.guest_No 
+inner join hotel as h on b.hotel_No=h.hotel_No where month(Date_From)=8 and h.city='New York'; 
+
+#12.List the hotel names and room numbers of any hotel rooms that have not been booked.
+select  h.name,r.hotel_no,r.room_No from room r left join booking b on r.room_No=b.room_No inner join hotel h on h.hotel_no=r.hotel_no where b.room_No is null;
+
+#13.List the hotel name and city of the hotel with the highest priced room.
+select h.name,h.city from hotel as h inner join room as r on h.hotel_No=r.hotel_No ;
+select room_No,hotel_No,price,
